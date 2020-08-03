9a392295576989d115a2507fc4b950c3105ec23d
Natalia Popova <cindy.n-popova@yandex.ru>
2020-08-03 21:45:57 +0300

    javascript correction

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.button__edit');
 let popupCloseButton = popup.querySelector('.popup__close');
+let nameInput = popup.querySelector('.popup__text_name');
+let jobInput =  popup.querySelector('.popup__text_description');
+let name = document.querySelector('.profile__name');
+let description = document.querySelector('.profile__description');
 
 function popupToogle () {
   popup.classList.toggle('popup_open');
+  nameInput.value = name.textContent;
+  jobInput.value = description.textContent;
 }
 
 popupOpenButton.addEventListener ('click', popupToogle);
@@ -13,12 +19,6 @@ popupCloseButton.addEventListener ('click', popupToogle);
 // Обработчик «отправки» формы
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    // Находим поля формы в DOM
-    let nameInput = popup.querySelector('.popup__text_name');
-    let jobInput =  popup.querySelector('.popup__text_description');
-    let name = document.querySelector('.profile__name');
-    let description = document.querySelector('.profile__description');
 
     name.textContent = nameInput.value;
     description.textContent = jobInput.value;
