8673e8369cd9c16f42b4a6ee8e4649b2e2691dd5
Natalia Popova <cindy.n-popova@yandex.ru>
2020-08-15 20:18:14 +0300

    open 2 forms edit+newItem

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,47 @@
-const popup = document.querySelector('.popup');
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupNewItem = document.querySelector('.popup_type_new-item');
 const popupOpenButton = document.querySelector('.profile__button-edit');
-const popupCloseButton = popup.querySelector('.popup__close');
-const nameInput = popup.querySelector('.popup__text_type_name');
-const jobInput =  popup.querySelector('.popup__text_type_description');
+const popupCloseButton = popupEdit.querySelector('.popup__close');
+const nameInput = popupEdit.querySelector('.popup__text_type_name');
+const jobInput =  popupEdit.querySelector('.popup__text_type_description');
 const name = document.querySelector('.profile__name');
 const description = document.querySelector('.profile__description');
+const popupNewItemOpenButton = document.querySelector('.profile__button-add');
 
+//работа с формой редактирования профайла
 function popupToogle () {
-  popup.classList.toggle('popup_open');
+  popupEdit.classList.toggle('popup_open');
   nameInput.value = name.textContent;
   jobInput.value = description.textContent;
 }
 
-popupOpenButton.addEventListener ('click', popupToogle);
-popupCloseButton.addEventListener ('click', popupToogle);
-
-
-// Обработчик «отправки» формы
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
@@ -25,4 +50,14 @@ function formSubmitHandler (evt) {
     popupToogle();
 }
 
-popup.addEventListener('submit', formSubmitHandler);
+popupOpenButton.addEventListener ('click', popupToogle);
+popupCloseButton.addEventListener ('click', popupToogle);
+popupEdit.addEventListener('submit', formSubmitHandler);
+
+//работа с формой добавления новых катинок
+
+function popupToogleNewItem () {
+    popupNewItem.classList.toggle('popup_open');
+  }
+  popupNewItemOpenButton.addEventListener('click', popupToogleNewItem);
+
