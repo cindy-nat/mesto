f7ca993bd40d1beeda03a3ab5bde7dae9bafe535
Natalia Popova <cindy.n-popova@yandex.ru>
2020-08-19 09:24:44 +0300

    new item popup_close + adding new item

--- scripts/main.js
+++ scripts/main.js
@@ -26,14 +26,12 @@ const initialCards = [
 ];
 
 const popupEdit = document.querySelector('.popup_type_edit');
-const popupNewItem = document.querySelector('.popup_type_new-item');
 const popupOpenButton = document.querySelector('.profile__button-edit');
 const popupCloseButton = popupEdit.querySelector('.popup__close');
 const nameInput = popupEdit.querySelector('.popup__text_type_name');
 const jobInput =  popupEdit.querySelector('.popup__text_type_description');
 const name = document.querySelector('.profile__name');
 const description = document.querySelector('.profile__description');
-const popupNewItemOpenButton = document.querySelector('.profile__button-add');
 
 //работа с формой редактирования профайла
 function popupToogle () {
@@ -56,8 +54,53 @@ popupEdit.addEventListener('submit', formSubmitHandler);
 
 //работа с формой добавления новых катинок
 
-function popupToogleNewItem () {
+const popupNewItemOpenButton = document.querySelector('.profile__button-add');
+const templateNewItem = document.querySelector('.new-item');
+const cards = document.querySelector('.cards');
+const popupNewItem = document.querySelector('.popup_type_new-item');
+const popupNewItemName = popupNewItem.querySelector('.popup__text_type_picture-name');
+const popupNewItemLink = popupNewItem.querySelector('.popup__text_type_link');
+const popupNewItemClose = popupNewItem.querySelector('.popup__close');
+
+
+  function popupToggleNewItem() {
     popupNewItem.classList.toggle('popup_open');
+    popupNewItemName.value='';
+    popupNewItemLink.value='';
   }
-  popupNewItemOpenButton.addEventListener('click', popupToogleNewItem);
+
+  function addItemtoContainer (name, link) {
+    const newItem =  templateNewItem.content.cloneNode(true);
+    const newItemPhoto = newItem.querySelector('.cards__photo');
+    newItemPhoto.src = link;
+    newItemPhoto.alt = name;
+    newItem.querySelector('.cards__name').textContent = name;
+
+    cards.prepend(newItem);
+  }
+
+  initialCards.forEach(function(item){
+    const newItem =  templateNewItem.content.cloneNode(true);
+    const newItemPhoto = newItem.querySelector('.cards__photo');
+    newItemPhoto.src = item.link;
+    newItemPhoto.alt = item.name;
+    newItem.querySelector('.cards__name').textContent = item.name;
+
+    cards.append(newItem);
+  });
+
+  popupNewItem.addEventListener('submit', event => {
+    event.preventDefault();
+    const newName = popupNewItemName.value;
+    const newItemLink = popupNewItemLink.value;
+    if(newName!=='' && newItemLink!=='') {
+      addItemtoContainer(newName, newItemLink);
+      popupToggleNewItem();
+      popupNewItem.reset();
+    }
+  })
+
+popupNewItemOpenButton.addEventListener('click', popupToggleNewItem);
+popupNewItemClose.addEventListener ('click', popupToggleNewItem);
+
 
