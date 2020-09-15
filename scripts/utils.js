b31db0ca78a5e7513fdaef747cf8a409e185781a
Natalia Popova <cindy.n-popova@yandex.ru>
2020-09-15 21:51:29 +0300

    changed esc function in order to remove listener

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,17 @@
 //общие обработчики
 // закрытие окна для всех функций
 export const closeModalWindow = (modalWindow) => {modalWindow.classList.remove('popup_opened');
-  document.removeEventListener('keyup', (evt) =>{closePopupEsc(modalWindow,evt);});
+  document.removeEventListener('keyup', closePopupEsc);
+}
 
-};
-function closePopupEsc (popup, event) {
-  if(event.key === "Escape" && popup.classList.contains('popup_opened')) closeModalWindow (popup);
+function closePopupEsc (event) {
+  if(event.key === "Escape") {
+    const openedPopup = document.querySelector('.popup_opened');
+    closeModalWindow(openedPopup);
+  }
 }
 
 // открытие окна для всех функций
 export const openModalWindow = (modalWindow) => {modalWindow.classList.add('popup_opened');
-  document.addEventListener('keyup', (evt) =>{closePopupEsc(modalWindow,evt);});
-};
+  document.addEventListener('keyup', closePopupEsc);
+}
