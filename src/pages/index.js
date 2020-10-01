58b33270d838495b5fc7a20d98b2255389cc9691
Natalia Popova <cindy.n-popova@yandex.ru>
2020-10-01 14:17:44 +0300

    removed additional creation of section class

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,10 +36,29 @@ popupOpenButton.addEventListener ('click', ()=> {
 const addCardFormValidator = new FormValidator(profileValidationClasses, popupNewItem);
   addCardFormValidator.enableValidation();
 
+//создание новых карточек
+const createCard = (item, renderList) => {
+  //создание класса для открытия картинки
+  const card = new Card(item, '.new-item', {handleCardClick: ()=>{
+      popupWithImage.open(item);
+    }});
+  const cardElement = card.generateCard();
+  renderList.addItem(cardElement);
+}
+
+//создание класса Section для отрисовки картинок
+const cardRenderer = new Section({
+    items: initialCards,
+    renderer: (item) => {
+      createCard(item, cardRenderer);
+    },
+  },
+  cardsContainer
+);
+cardRenderer.renderItems(); //добавление карточек из массива
+
   //создание класса попапа для карточек
 const popupNewItemForm = new PopupWithForm(popupNewItem, {submitFunction: (inputValues) => {
-      const cardRenderer = new Section({
-        items: []}, cardsContainer);
       createCard(inputValues,cardRenderer);
       popupNewItemForm.close();}});
 popupNewItemForm.setEventListeners(); //установка слушателей для попапа карточек
@@ -48,32 +67,9 @@ popupNewItemForm.setEventListeners(); //установка слушателей 
 const popupWithImage = new PopupWithImage(popupImage);
 popupWithImage.setEventListeners();
 
- //создание новых карточек
-const createCard = (item, renderList) => {
-  //создание класса для открытия картинки
-  const card = new Card(item, '.new-item', {handleCardClick: ()=>{
-      popupWithImage.open(item);
-  }});
-  const cardElement = card.generateCard();
-  renderList.addItem(cardElement);
-}
-
-//добавление карточек из массива
-const cardsList = new Section({
-    items: initialCards,
-    renderer: (item) => {
-      createCard(item, cardsList);
-    },
-  },
-  cardsContainer
-);
-cardsList.renderItems();
-
 //открытие попапа
 popupNewItemOpenButton.addEventListener('click', () => {
   popupNewItemForm.open();
-  //popupNewItemName.value='';
-  //popupNewItemLink.value='';
   addCardFormValidator.resetForm();//сброс валидации формы
 });
 
