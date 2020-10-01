155d7b15a3e1c36b904234b8cc7fe8a5ca230326
Natalia Popova <cindy.n-popova@yandex.ru>
2020-10-01 14:03:48 +0300

    renamed scripts to components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import Card from "../scripts/Card.js";
-import FormValidator from "../scripts/FormValidator.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
 import {initialCards, popupImage, profileValidationClasses, popupEdit, jobInput, nameInput, popupOpenButton,
   profileDescription, profileName, popupNewItem, popupNewItemLink, cardsContainer, popupNewItemName,
   popupNewItemOpenButton} from "../utils/constants.js";
-import Section from "../scripts/Section.js";
-import PopupWithForm from "../scripts/PopupWithForm.js";
-import PopupWithImage from "../scripts/PopupWithImage.js";
-import UserInfo from "../scripts/UserInfo.js";
+import Section from "../components/Section.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import UserInfo from "../components/UserInfo.js";
 import './index.css';
 
 //создание класса UserInfo
